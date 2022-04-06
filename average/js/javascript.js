4fa40be383bf3a3be1427d19aedcd032d910e482
snipee3007 <102203715+snipee3007@users.noreply.github.com>
2022-04-06 10:41:36 +0700

    Update javascript.js

--- average/js/javascript.js
+++ average/js/javascript.js
@@ -1,26 +1,26 @@
 var avg = [];
+var sum = 0;
 
 function reset(){
 	avg = [];
+	sum = 0;
 	document.getElementById('cout').innerHTML = "Sum of: " + avg;
 	document.getElementById('Avg').innerHTML = "Average is: " + avg;
 }
 
 function average(){
 	var ans = prompt("Add a number: ");
-	var sum = 0;
+
 	if (ans.length != 0)
 	{
 		avg.push(" " + ans);
+		sum += ans / 1;
+		document.getElementById('cout').innerHTML = "Sum of " + avg.length +" number(s): "+ avg;
+		var a = ans;
+		if (avg.length > 1)
+		{
+			a = sum / (avg.length);
+		}
+		document.getElementById('Avg').innerHTML = "Average is: " + a;
 	}
-    document.getElementById('cout').innerHTML = "Sum of: " + avg;
-	for (i = 0; i < avg.length; ++i)
-	{
-		sum += avg[i] / 1;
-	}
-	if (avg.length > 1)
-	{
-		sum = sum / (avg.length);
-	}
-	document.getElementById('Avg').innerHTML = "Average is: " + sum;
 }
